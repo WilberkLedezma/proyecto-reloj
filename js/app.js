8b79092d9cb0394e855390a801a3eee2f03ceeb2
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2021-06-06 02:05:39 +0200

    Subiendo un proyectico reloj

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// function mostrarReloj()
+// {
+// 	let fecha = new Date();
+// 	let hr = fecha.getHours();
+// 	let min = fecha.getMinutes();
+// 	let seg = fecha.getSeconds();
+// 	document.getElementById("fecha").innerHTML = `${hr}:${min}:${seg}`; 
+
+// 	const meses = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+// 	const dias = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+
+// 	let diaDeLaSemana = dias[fecha.getDay()];
+// }
+
+// const formatoHora = (hora)=>
+// {
+// 	if(hora < 10)
+// 	{
+// 		hora = "0" + hora;
+// 		return hora;
+// 	}
+// }
+
+// setInterval(mostrarReloj, 1000);
+
+
